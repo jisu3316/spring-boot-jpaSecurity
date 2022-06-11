3d57bdf4c0e7427013a6547d5bbfe52c425cfc5a
Jisu Kim <kimjisu3316@naver.com>
2022-06-11 23:55:18 +0900

    환경설정완료

---
+++ src/main/java/com/jisu/jpashopSecurity/JpashopSecurityApplication.java
@@ -0,0 +1,13 @@
+package com.jisu.jpashopSecurity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JpashopSecurityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JpashopSecurityApplication.class, args);
+	}
+
+}
